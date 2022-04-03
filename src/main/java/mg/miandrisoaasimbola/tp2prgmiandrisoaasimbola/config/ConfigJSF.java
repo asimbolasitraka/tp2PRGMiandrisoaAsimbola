8f513027a423dfd52f58f3687300cea4e2424650
MOI <MOI@Asus>
2022-04-03 21:48:34 +0300

    Exercice 1 : analyse d'un post tout simple création d'un backing bean création de la page formulaire et affichage

---
+++ src/main/java/mg/miandrisoaasimbola/tp2prgmiandrisoaasimbola/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package mg.miandrisoaasimbola.tp2prgmiandrisoaasimbola.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * Classe de configuration des JSF
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
